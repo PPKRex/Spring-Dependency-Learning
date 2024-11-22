f52ea3c18d5728b9ce5cab2a1d35414f4a19ab98
PPKRex <josecarlosppk7@gmail.com>
2024-11-22 13:01:57 +0100

    Test Practice

--- src/test/java/ppk/springsecondproyect/spring_6_Dependency/Spring6DependencyApplicationTests.java
+++ src/test/java/ppk/springsecondproyect/spring_6_Dependency/Spring6DependencyApplicationTests.java
@@ -1,11 +1,31 @@
 package ppk.springsecondproyect.spring_6_Dependency;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import ppk.springsecondproyect.spring_6_Dependency.controllers.MyController;
 
 @SpringBootTest
 class Spring6DependencyApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowireOfController(){
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx(){
+		MyController controller = (MyController) applicationContext.getBean(MyController.class);
+
+		System.out.println(controller.sayHello());
+	}
 	@Test
 	void contextLoads() {
 	}
